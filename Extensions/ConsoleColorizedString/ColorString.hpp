0c746a9befade9ae1da198c96812d83e9fef048c
COGMLS <mlsmatheus@hotmail.com>
2024-04-12 15:13:16 -0300

    Fix comments.

--- Extensions/ConsoleColorizedString/ColorString.hpp
+++ Extensions/ConsoleColorizedString/ColorString.hpp
@@ -26,8 +26,8 @@ std::string ColorizeString(std::string str, ConsoleBackgroundColor bColor);
 std::string ColorizeString(std::string str, ConsoleForegroundColor fColor, ConsoleBackgroundColor bColor);
 
 /// @brief Remove the colors from a string (background and foreground)
-/// @param str String to be uncolorized
-/// @return Return the uncolorized string if is successfull, otherwise will return the same string if failed
+/// @param str String to be uncolonized
+/// @return Return the uncolonized string if is successful, otherwise will return the same string if failed
 std::string UnColorizeString(std::string str);
 
 #endif // !COLORSTRING_HPP
--- Extensions/ConsoleColorizedString/ColorWstring.hpp
+++ Extensions/ConsoleColorizedString/ColorWstring.hpp
@@ -26,8 +26,8 @@ std::wstring ColorizeWstring(std::wstring wstr, ConsoleBackgroundColor bColor);
 std::wstring ColorizeWstring(std::wstring wstr, ConsoleForegroundColor fColor, ConsoleBackgroundColor bColor);
 
 /// @brief Remove the colors from a string (background and foreground)
-/// @param wstr String to be uncolorized
-/// @return Return the uncolorized string if is successfull, otherwise will return the same string if failed
+/// @param wstr String to be uncolonized
+/// @return Return the uncolonized string if is successful, otherwise will return the same string if failed
 std::wstring UnColorizeWstring(std::wstring wstr);
 
 #endif // !COLORWSTRING_HPP
