eccb5c2652afa447cf147a1cbad230fac2294114
COGMLS <mlsmatheus@hotmail.com>
2024-09-21 16:47:15 -0300

    Changed the defined names. Added Linux support. Added non wide string functions. Updated all existing functions to use W on the end of their name. Added proper use of wide strings (fix for Linux Consoles).

--- Extensions/Libs/ConsoleExtInterface/ConsoleExtInterface.cpp
+++ Extensions/Libs/ConsoleExtInterface/ConsoleExtInterface.cpp
@@ -1,61 +1,67 @@
 #include "ConsoleExtInterface.hpp"
 
-void SetCursorPosition(int x, int y)
+void ConsoleExt::SetCursorPosition(int x, int y)
 {
-	std::string s = SETCURSORPOS(x, y);
+	std::string s = CONSOLE_EXT_SET_CURSOR_POS(x, y);
 	std::cout << s;
 }
 
-void DrawBorder(int startPosX, int startPosY, int maxWidth, int maxHeight, int setCursorPosX, int setCursorPosY)
+void ConsoleExt::SetCursorPositionW(int x, int y)
+{
+	std::wstring s = CONSOLE_EXT_SET_CURSOR_POS_W(x, y);
+	std::wcout << s;
+}
+
+void ConsoleExt::DrawBorder(int startPosX, int startPosY, int maxWidth, int maxHeight, int setCursorPosX, int setCursorPosY)
 {
 	//int maxHeight = 30;
 	//int maxWidth = 110;
 
 	if (startPosX <= 0 || startPosY <= 0)
 	{
-		std::exception e("Start position parameter(s) is/are less than 1!");
-		std::wcerr << e.what() << std::endl;
+		ConsoleExt::exception e("Start position parameter(s) is/are less than 1!");
+		std::cerr << e.what() << std::endl;
 	}
 
 	if (startPosX >= maxWidth || startPosY >= maxHeight)
 	{
-		std::exception e("Start position parameter(s) is/are equal to maximum value(s)!");
-		std::wcerr << e.what() << std::endl;
+		ConsoleExt::exception e("Start position parameter(s) is/are equal to maximum value(s)!");
+		std::cerr << e.what() << std::endl;
 	}
 
-	std::wcout << SAVECURSORPOS;
+	std::cout << CONSOLE_EXT_SAVE_CURSOR_POS;
 
 	int actualCursorPosX = 0;
-	int actualCursosPosY = 0;
+	int actualCursorPosY = 0;
 
 	for (int y = startPosY; y <= maxHeight; y++)
 	{
 		for (int x = startPosX; x <= maxWidth; x++)
 		{
-			SetCursorPosition(x, y);
+			ConsoleExt::SetCursorPosition(x, y);
 
 			// Make a complete draw on first and last console lines:
 			if (y == startPosY || y == maxHeight)
 			{
 				if (x == startPosX && y == startPosY)
 				{
-					std::wcout << BORDER_TOP_LEFT_CORNER; // Top Left corner (original symbol: u250F)
+					std::cout << BORDER_TOP_LEFT_CORNER; // Top Left corner (original symbol: u250F)
 				}
 				else if (x == maxWidth && y == startPosY)
 				{
-					std::wcout << BORDER_TOP_RIGHT_CORNER;    // Top right corner (original symbol: u2513)
+					std::cout << BORDER_TOP_RIGHT_CORNER;    // Top right corner (original symbol: u2513)
 				}
 				else if (x == startPosX && y == maxHeight)
 				{
-					std::wcout << BORDER_BOTTOM_LEFT_CORNER;    // Bottom left corner (original symbol: u2517)
+					std::cout << BORDER_BOTTOM_LEFT_CORNER;    // Bottom left corner (original symbol: u2517)
 				}
 				else if (x == maxWidth && y == maxHeight)
 				{
-					std::wcout << BORDER_BOTTOM_RIGHT_CORNER; // Bottom right corner (original symbol: u251B)
+					std::cout << BORDER_BOTTOM_RIGHT_CORNER; // Bottom right corner (original symbol: u251B)
 				}
 				else
 				{
-					std::wcout << BORDER_LINE_HORIZONTAL; // Line on top and bottom (original symbol: u2501)
+					std::cout << BORDER_LINE_HORIZONTAL; // Line on top and bottom (original symbol: u2501)
 				}
 			}
 			else
@@ -63,7 +69,7 @@ void DrawBorder(int startPosX, int startPosY, int maxWidth, int maxHeight, int s
 				// Make a border side draw
 				if (x == startPosX || x == maxWidth)
 				{
-					std::wcout << BORDER_LINE_VERTICAL; // Right and left column (original symbol: u2503)
+					std::cout << BORDER_LINE_VERTICAL; // Right and left column (original symbol: u2503)
 				}
 			}
 		}
@@ -76,20 +82,103 @@ void DrawBorder(int startPosX, int startPosY, int maxWidth, int maxHeight, int s
 
 	if (setCursorPosY >= 1 && setCursorPosY <= maxHeight)
 	{
-		actualCursosPosY = setCursorPosY;
+		actualCursorPosY = setCursorPosY;
 	}
 
 	if (setCursorPosX >= 1 && setCursorPosY >= 1)
 	{
-		SetCursorPosition(actualCursorPosX, actualCursosPosY);
+		ConsoleExt::SetCursorPosition(actualCursorPosX, actualCursorPosY);
 	}
 	else
 	{
-		std::wcout << RESTORECURSORPOS;
+		std::cout << CONSOLE_EXT_RESTORE_CURSOR_POS;
 	}
 }
 
-void DrawHorizontalLine(int xStart, int yStart, int count, bool useStartCrossLine, bool useEndCrossLine)
+void ConsoleExt::DrawBorderW(int startPosX, int startPosY, int maxWidth, int maxHeight, int setCursorPosX, int setCursorPosY)
+{
+	//int maxHeight = 30;
+	//int maxWidth = 110;
+
+	if (startPosX <= 0 || startPosY <= 0)
+	{
+		ConsoleExt::exception e("Start position parameter(s) is/are less than 1!");
+		std::wcerr << e.what() << std::endl;
+	}
+
+	if (startPosX >= maxWidth || startPosY >= maxHeight)
+	{
+		ConsoleExt::exception e("Start position parameter(s) is/are equal to maximum value(s)!");
+		std::wcerr << e.what() << std::endl;
+	}
+
+	std::wcout << CONSOLE_EXT_SAVE_CURSOR_POS_W;
+
+	int actualCursorPosX = 0;
+	int actualCursorPosY = 0;
+
+	for (int y = startPosY; y <= maxHeight; y++)
+	{
+		for (int x = startPosX; x <= maxWidth; x++)
+		{
+			ConsoleExt::SetCursorPositionW(x, y);
+
+			// Make a complete draw on first and last console lines:
+			if (y == startPosY || y == maxHeight)
+			{
+				if (x == startPosX && y == startPosY)
+				{
+					std::wcout << BORDER_TOP_LEFT_CORNER_W; // Top Left corner (original symbol: u250F)
+				}
+				else if (x == maxWidth && y == startPosY)
+				{
+					std::wcout << BORDER_TOP_RIGHT_CORNER_W;    // Top right corner (original symbol: u2513)
+				}
+				else if (x == startPosX && y == maxHeight)
+				{
+					std::wcout << BORDER_BOTTOM_LEFT_CORNER_W;    // Bottom left corner (original symbol: u2517)
+				}
+				else if (x == maxWidth && y == maxHeight)
+				{
+					std::wcout << BORDER_BOTTOM_RIGHT_CORNER_W; // Bottom right corner (original symbol: u251B)
+				}
+				else
+				{
+					std::wcout << BORDER_LINE_HORIZONTAL_W; // Line on top and bottom (original symbol: u2501)
+				}
+			}
+			else
+			{
+				// Make a border side draw
+				if (x == startPosX || x == maxWidth)
+				{
+					std::wcout << BORDER_LINE_VERTICAL_W; // Right and left column (original symbol: u2503)
+				}
+			}
+		}
+	}
+
+	if (setCursorPosX >= 1 && setCursorPosX <= maxWidth)
+	{
+		actualCursorPosX = setCursorPosX;
+	}
+
+	if (setCursorPosY >= 1 && setCursorPosY <= maxHeight)
+	{
+		actualCursorPosY = setCursorPosY;
+	}
+
+	if (setCursorPosX >= 1 && setCursorPosY >= 1)
+	{
+		ConsoleExt::SetCursorPositionW(actualCursorPosX, actualCursorPosY);
+	}
+	else
+	{
+		std::wcout << CONSOLE_EXT_RESTORE_CURSOR_POS_W;
+	}
+}
+
+void ConsoleExt::DrawHorizontalLine(int xStart, int yStart, int count, bool useStartCrossLine, bool useEndCrossLine)
 {
 	if (xStart <= 0)
 	{
@@ -103,38 +192,154 @@ void DrawHorizontalLine(int xStart, int yStart, int count, bool useStartCrossLin
 
 	if (count < 0)
 	{
-		std::exception e("Count out of range!");
-		std::wcerr << e.what() << std::endl;
+		ConsoleExt::exception e("Count out of range!");
+		std::cerr << e.what() << std::endl;
 	}
 
-	std::wcout << SAVECURSORPOS;
+	std::cout << CONSOLE_EXT_SAVE_CURSOR_POS;
 
-	SetCursorPosition(xStart, yStart);
+	ConsoleExt::SetCursorPosition(xStart, yStart);
 
 	for (int i = xStart; i <= count; i++)
 	{
 		if (useStartCrossLine && i == xStart)
 		{
-			std::wcout << BORDER_CROSS_LINE;
+			std::cout << BORDER_CROSS_LINE;
 		}
 		else if (useEndCrossLine && i == count)
 		{
-			std::wcout << BORDER_CROSS_LINE;
+			std::cout << BORDER_CROSS_LINE;
 		}
 		else
 		{
-			std::wcout << BORDER_LINE_HORIZONTAL;
+			std::cout << BORDER_LINE_HORIZONTAL;
 		}
 	}
 
-	std::wcout << RESTORECURSORPOS;
+	std::cout << CONSOLE_EXT_RESTORE_CURSOR_POS;
 }
 
-void DrawProgressBar(int progressBarXPos, int progressBarYPos, int maxWidth, int maxValue, int actualValue)
+void ConsoleExt::DrawHorizontalLineW(int xStart, int yStart, int count, bool useStartCrossLine, bool useEndCrossLine)
 {
-	std::wcout << SAVECURSORPOS;
+	if (xStart <= 0)
+	{
+		xStart = 1;
+	}
 
-	SetCursorPosition(progressBarXPos, progressBarYPos);
+	if (yStart <= 0)
+	{
+		yStart = 1;
+	}
+
+	if (count < 0)
+	{
+		ConsoleExt::exception e("Count out of range!");
+		std::wcerr << e.what() << std::endl;
+	}
+
+	std::wcout << CONSOLE_EXT_SAVE_CURSOR_POS_W;
+
+	ConsoleExt::SetCursorPositionW(xStart, yStart);
+
+	for (int i = xStart; i <= count; i++)
+	{
+		if (useStartCrossLine && i == xStart)
+		{
+			std::wcout << BORDER_CROSS_LINE_W;
+		}
+		else if (useEndCrossLine && i == count)
+		{
+			std::wcout << BORDER_CROSS_LINE_W;
+		}
+		else
+		{
+			std::wcout << BORDER_LINE_HORIZONTAL_W;
+		}
+	}
+
+	std::wcout << CONSOLE_EXT_RESTORE_CURSOR_POS_W;
+}
+
+void ConsoleExt::DrawProgressBar(int progressBarXPos, int progressBarYPos, int maxWidth, int maxValue, int actualValue)
+{
+	std::cout << CONSOLE_EXT_SAVE_CURSOR_POS;
+
+	ConsoleExt::SetCursorPosition(progressBarXPos, progressBarYPos);
+
+	// The progress bar should look like: [*****    ] | (###|###)
+
+	std::string p0 = "[";
+	std::string p1 = "] | (";
+	std::string p2 = "|";
+	std::string p3 = ")";
+	std::string actualValStr = std::to_string(actualValue);
+	std::string maxValStr = std::to_string(maxValue);
+
+	std::string info = p1 + actualValStr + p2 + maxValStr + p3;
+
+	// Adapt the progress bar and progress bar informations
+	int maxInfoSize = p0.size() + p1.size() + p2.size() + p3.size() + 2 * maxValStr.size(); // Use 2 maxValue to reserve the char space when actualSize is equal.
+
+	// Progress bar chars reserved:
+	int usableProgressBar = maxWidth - maxInfoSize;
+
+	//int tmpMaxWidthUsed = maxValue + maxInfoSize;
+
+	// Generate the progress bar proportion based on maxWidth and usableProgressBar info:
+
+	/**
+	 * maxValue (100%) = usableProgressBar
+	 * 
+	 * actualValue = actualValue / maxValue
+	 * chars2Render = actualVar * usableProgressBar
+	*/
+
+	float valPerChar = (float)maxValue / (float)usableProgressBar;
+
+	// Redefine the actualValue based on usableProgressBar value:
+
+	float actualValPercent = static_cast<float>(actualValue) / static_cast<float>(maxValue);
+	float chars2Render = actualValPercent * usableProgressBar;
+	actualValue = static_cast<int>(chars2Render);
+
+	// Redefine the maxValue presentation with the proportion:
+	maxValue = usableProgressBar;
+
+	// Clean the console line segment
+	for (int i = (maxWidth - progressBarXPos); i < maxWidth; i++)
+	{
+		std::cout << " ";
+	}
+
+	ConsoleExt::SetCursorPosition(progressBarXPos, progressBarYPos);
+
+	// Draw the progress bar:
+
+	std::cout << "[";
+
+	for (int i = 0; i <= maxValue; i++)
+	{
+		if (i <= actualValue)
+		{
+			std::cout << "\u25a0";
+			//std::printf("%lc", '\u25a0');
+		}
+		else
+		{
+			std::cout << " ";
+		}
+	}
+
+	std::cout << info;
+
+	std::cout << CONSOLE_EXT_RESTORE_CURSOR_POS;
+}
+
+void ConsoleExt::DrawProgressBarW(int progressBarXPos, int progressBarYPos, int maxWidth, int maxValue, int actualValue)
+{
+	std::wcout << CONSOLE_EXT_SAVE_CURSOR_POS_W;
+
+	ConsoleExt::SetCursorPositionW(progressBarXPos, progressBarYPos);
 
 	// The progress bar should look like: [*****    ] | (###|###)
 
@@ -147,7 +352,7 @@ void DrawProgressBar(int progressBarXPos, int progressBarYPos, int maxWidth, int
 
 	std::wstring info = p1 + actualValStr + p2 + maxValStr + p3;
 
-	// Adapte the progress bar and progress bar informations
+	// Adapt the progress bar and progress bar informations
 	int maxInfoSize = p0.size() + p1.size() + p2.size() + p3.size() + 2 * maxValStr.size(); // Use 2 maxValue to reserve the char space when actualSize is equal.
 
 	// Progress bar chars reserved:
@@ -181,7 +386,7 @@ void DrawProgressBar(int progressBarXPos, int progressBarYPos, int maxWidth, int
 		std::wcout << L" ";
 	}
 
-	SetCursorPosition(progressBarXPos, progressBarYPos);
+	ConsoleExt::SetCursorPositionW(progressBarXPos, progressBarYPos);
 
 	// Draw the progress bar:
 
@@ -202,5 +407,5 @@ void DrawProgressBar(int progressBarXPos, int progressBarYPos, int maxWidth, int
 
 	std::wcout << info;
 
-	std::cout << RESTORECURSORPOS;
+	std::wcout << CONSOLE_EXT_RESTORE_CURSOR_POS_W;
 }
--- Extensions/Libs/ConsoleExtInterface/ConsoleExtInterface.hpp
+++ Extensions/Libs/ConsoleExtInterface/ConsoleExtInterface.hpp
@@ -1,89 +1,170 @@
 #pragma once
 
-#ifndef CONSOLEEXT_HPP
-#define CONSOLEEXT_HPP
+#ifndef CONSOLE_EXT_GRAPHIC_INTERFACE_HPP
+#define CONSOLE_EXT_GRAPHIC_INTERFACE_HPP
 
 #ifdef WIN32
-	#ifdef CONSOLEEXTINTERFACE_EXPORTS
-		#define CONSOLEEXTINTERFACE_LIB_API __declspec(dllexport)
+	#ifdef CONSOLE_EXT_GRAPHIC_INTERFACE_EXPORTS
+		#define CONSOLE_EXT_GRAPHIC_INTERFACE_LIB_API __declspec(dllexport)
 	#else
-		#define CONSOLEEXTINTERFACE_LIB_API __declspec(dllimport)
-	#endif //!CONSOLEEXTINTERFACE_EXPORTS
+		#define CONSOLE_EXT_GRAPHIC_INTERFACE_LIB_API __declspec(dllimport)
+	#endif //!CONSOLE_EXT_GRAPHIC_INTERFACE_EXPORTS
 
 	#pragma warning (disable : 4251)
 	#pragma warning (disable : 4273)
+#else
+	#if __GNUC__ >= 4
+		#ifdef CONSOLE_EXT_GRAPHIC_INTERFACE_EXPORTS
+			#define CONSOLE_EXT_GRAPHIC_INTERFACE_LIB_API __attribute__((visibility("default")))
+		#else
+			#define CONSOLE_EXT_GRAPHIC_INTERFACE_LIB_API __attribute__((visibility("default")))
+		#endif //!CONSOLE_EXT_GRAPHIC_INTERFACE_EXPORTS
+	#else
+		#ifdef CONSOLE_EXT_GRAPHIC_INTERFACE_EXPORTS
+			#define CONSOLE_EXT_GRAPHIC_INTERFACE_LIB_API
+		#else
+			#define CONSOLE_EXT_GRAPHIC_INTERFACE_LIB_API
+		#endif //!CONSOLE_EXT_GRAPHIC_INTERFACE_EXPORTS
+	#endif
 #endif // !WIN32
 
 #include <iostream>
 #include <string>
-#include <exception>
+
+#include "ConsoleExtException.hpp"
 
 #define ESC "\x1B"
+#define ESC_W L"\x1B"
+
 #define CSI ESC"["
+#define CSI_W ESC_W L"["
 
-#define SAVECURSORPOS ESC" 7"
-#define RESTORECURSORPOS ESC" 8"
+#define CONSOLE_EXT_SAVE_CURSOR_POS ESC" 7"
+#define CONSOLE_EXT_SAVE_CURSOR_POS_W ESC_W L" 7"
 
-#define SETCURSORPOS(x,y) CSI + std::to_string(y) + ";" + std::to_string(x) + "H";
+#define CONSOLE_EXT_RESTORE_CURSOR_POS ESC" 8"
+#define CONSOLE_EXT_RESTORE_CURSOR_POS_W ESC_W L" 8"
 
-#define DONT_USE_CHARS
+#define CONSOLE_EXT_SET_CURSOR_POS(x,y) CSI + std::to_string(y) + ";" + std::to_string(x) + "H";
+#define CONSOLE_EXT_SET_CURSOR_POS_W(x,y) CSI_W + std::to_wstring(y) + L";" + std::to_wstring(x) + L"H";
 
-#ifdef DONT_USE_CHARS
-	#define BORDER_TOP_LEFT_CORNER				L"\u250F"
-	#define BORDER_TOP_RIGHT_CORNER				L"\u2513"
-	#define BORDER_BOTTOM_LEFT_CORNER			L"\u2517"
-	#define BORDER_BOTTOM_RIGHT_CORNER			L"\u251B"
-	#define BORDER_LINE_HORIZONTAL				L"\u2501"
-	#define BORDER_LINE_VERTICAL				L"\u2503"
-	#define BORDER_CROSS_LINE					L"\u254B"
+#define USE_UNICODE_CHARS
+
+#ifdef USE_UNICODE_CHARS
+	#define BORDER_TOP_LEFT_CORNER				"\u250F"
+	#define BORDER_TOP_RIGHT_CORNER				"\u2513"
+	#define BORDER_BOTTOM_LEFT_CORNER			"\u2517"
+	#define BORDER_BOTTOM_RIGHT_CORNER			"\u251B"
+	#define BORDER_LINE_HORIZONTAL				"\u2501"
+	#define BORDER_LINE_VERTICAL				"\u2503"
+	#define BORDER_CROSS_LINE					"\u254B"
+
+	#define BORDER_TOP_LEFT_CORNER_W			L"\u250F"
+	#define BORDER_TOP_RIGHT_CORNER_W			L"\u2513"
+	#define BORDER_BOTTOM_LEFT_CORNER_W			L"\u2517"
+	#define BORDER_BOTTOM_RIGHT_CORNER_W		L"\u251B"
+	#define BORDER_LINE_HORIZONTAL_W			L"\u2501"
+	#define BORDER_LINE_VERTICAL_W				L"\u2503"
+	#define BORDER_CROSS_LINE_W					L"\u254B"
 #else
-	#define BORDER_TOP_LEFT_CORNER				L"/"
-	#define BORDER_TOP_RIGHT_CORNER				L"\\"
-	#define BORDER_BOTTOM_LEFT_CORNER			L"\\"
-	#define BORDER_BOTTOM_RIGHT_CORNER			L"/"
-	#define BORDER_LINE_HORIZONTAL				L"-"
-	#define BORDER_LINE_VERTICAL				L"|"
-	#define BORDER_CROSS_LINE					L"+"
+	#define BORDER_TOP_LEFT_CORNER				"/"
+	#define BORDER_TOP_RIGHT_CORNER				"\\"
+	#define BORDER_BOTTOM_LEFT_CORNER			"\\"
+	#define BORDER_BOTTOM_RIGHT_CORNER			"/"
+	#define BORDER_LINE_HORIZONTAL				"-"
+	#define BORDER_LINE_VERTICAL				"|"
+	#define BORDER_CROSS_LINE					"+"
+
+	#define BORDER_TOP_LEFT_CORNER_W			L"/"
+	#define BORDER_TOP_RIGHT_CORNER_W			L"\\"
+	#define BORDER_BOTTOM_LEFT_CORNER_W			L"\\"
+	#define BORDER_BOTTOM_RIGHT_CORNER_W		L"/"
+	#define BORDER_LINE_HORIZONTAL_W			L"-"
+	#define BORDER_LINE_VERTICAL_W				L"|"
+	#define BORDER_CROSS_LINE_W					L"+"
 #endif // !USE_UNICODE_CHARS
 
-/**
- * @brief Set the console cursor's position
- * @param x Position on X-axis
- * @param y Position on Y-axis
- */
-CONSOLEEXTINTERFACE_LIB_API void SetCursorPosition(int x, int y);
+namespace ConsoleExt
+{
+	/**
+	 * @brief Set the console cursor's position
+	 * @param x Position on X-axis
+	 * @param y Position on Y-axis
+	 */
+	CONSOLE_EXT_GRAPHIC_INTERFACE_LIB_API void SetCursorPosition(int x, int y);
+	
+	/**
+	 * @brief Set the console cursor's position
+	 * @param x Position on X-axis
+	 * @param y Position on Y-axis
+	 */
+	CONSOLE_EXT_GRAPHIC_INTERFACE_LIB_API void SetCursorPositionW(int x, int y);
 
-/**
- * @brief Draw a border in the console screen
- * @param startPosX Define the start position on X-axis
- * @param startPosY Define the start position on Y-axis
- * @param maxWidth Define the end of the border on X-axis
- * @param maxHeight Define the end of the border on Y-axis
- * @param setCursorPosX Define the cursor position after draw the border position on X-axis
- * @param setCursorPosY Define the cursor position after draw the border position on Y-axis
- * @note startPosX and startPosY need be greater than zero.
- * @note If setCursorPosX or setCursorPosY holds zero or a negative value, the cursor will be restored to last position before draw the border.
- */
-CONSOLEEXTINTERFACE_LIB_API void DrawBorder(int startPosX = 1, int startPosY = 1, int maxWidth = 110, int maxHeight = 30, int setCursorPosX = 0, int setCursorPosY = 0);
+	/**
+	 * @brief Draw a border in the console screen
+	 * @param startPosX Define the start position on X-axis
+	 * @param startPosY Define the start position on Y-axis
+	 * @param maxWidth Define the end of the border on X-axis
+	 * @param maxHeight Define the end of the border on Y-axis
+	 * @param CONSOLE_EXT_SET_CURSOR_POSX Define the cursor position after draw the border position on X-axis
+	 * @param CONSOLE_EXT_SET_CURSOR_POSY Define the cursor position after draw the border position on Y-axis
+	 * @note startPosX and startPosY need be greater than zero.
+	 * @note If CONSOLE_EXT_SET_CURSOR_POSX or CONSOLE_EXT_SET_CURSOR_POSY holds zero or a negative value, the cursor will be restored to last position before draw the border.
+	 */
+	CONSOLE_EXT_GRAPHIC_INTERFACE_LIB_API void DrawBorder(int startPosX = 1, int startPosY = 1, int maxWidth = 110, int maxHeight = 30, int CONSOLE_EXT_SET_CURSOR_POSX = 0, int CONSOLE_EXT_SET_CURSOR_POSY = 0);
 
-/**
- * @brief Draw a horizontal line in the console screen
- * @param xStart Start position on X-axis
- * @param yStart Start position on Y-axis
- * @param count How many chars on console screen will be used to the line
- * @param useStartCrossLine Use a cross line mark
- * @param useEndCrossLine Use a end cross line mark
- */
-CONSOLEEXTINTERFACE_LIB_API void DrawHorizontalLine(int xStart, int yStart, int count, bool useStartCrossLine, bool useEndCrossLine);
+	/**
+	 * @brief Draw a border in the console screen
+	 * @param startPosX Define the start position on X-axis
+	 * @param startPosY Define the start position on Y-axis
+	 * @param maxWidth Define the end of the border on X-axis
+	 * @param maxHeight Define the end of the border on Y-axis
+	 * @param CONSOLE_EXT_SET_CURSOR_POSX Define the cursor position after draw the border position on X-axis
+	 * @param CONSOLE_EXT_SET_CURSOR_POSY Define the cursor position after draw the border position on Y-axis
+	 * @note startPosX and startPosY need be greater than zero.
+	 * @note If CONSOLE_EXT_SET_CURSOR_POSX or CONSOLE_EXT_SET_CURSOR_POSY holds zero or a negative value, the cursor will be restored to last position before draw the border.
+	 */
+	CONSOLE_EXT_GRAPHIC_INTERFACE_LIB_API void DrawBorderW(int startPosX = 1, int startPosY = 1, int maxWidth = 110, int maxHeight = 30, int CONSOLE_EXT_SET_CURSOR_POSX = 0, int CONSOLE_EXT_SET_CURSOR_POSY = 0);
 
-/**
- * @brief Draw a progress bar with a counter and the final value to be reached
- * @param progressBarXPos Define the progress bar position on X-axis
- * @param progressBarYPor Define the progress bar position on Y-axis
- * @param maxWidth Define the maximum characters will be used to progress bar and counter informations
- * @param maxValue Final value that need be reached
- * @param actualValue Actual value in progress bar. NOTE: The progress bar need be called againg to update the console informations
- */
-CONSOLEEXTINTERFACE_LIB_API void DrawProgressBar(int progressBarXPos, int progressBarYPor, int maxWidth, int maxValue, int actualValue);
+	/**
+	 * @brief Draw a horizontal line in the console screen
+	 * @param xStart Start position on X-axis
+	 * @param yStart Start position on Y-axis
+	 * @param count How many chars on console screen will be used to the line
+	 * @param useStartCrossLine Use a cross line mark
+	 * @param useEndCrossLine Use a end cross line mark
+	 */
+	CONSOLE_EXT_GRAPHIC_INTERFACE_LIB_API void DrawHorizontalLine(int xStart, int yStart, int count, bool useStartCrossLine, bool useEndCrossLine);
 
-#endif //!CONSOLEEXT_HPP
+	/**
+	 * @brief Draw a horizontal line in the console screen
+	 * @param xStart Start position on X-axis
+	 * @param yStart Start position on Y-axis
+	 * @param count How many chars on console screen will be used to the line
+	 * @param useStartCrossLine Use a cross line mark
+	 * @param useEndCrossLine Use a end cross line mark
+	 */
+	CONSOLE_EXT_GRAPHIC_INTERFACE_LIB_API void DrawHorizontalLineW(int xStart, int yStart, int count, bool useStartCrossLine, bool useEndCrossLine);
+
+	/**
+	 * @brief Draw a progress bar with a counter and the final value to be reached
+	 * @param progressBarXPos Define the progress bar position on X-axis
+	 * @param progressBarYPor Define the progress bar position on Y-axis
+	 * @param maxWidth Define the maximum characters will be used to progress bar and counter informations
+	 * @param maxValue Final value that need be reached
+	 * @param actualValue Actual value in progress bar. NOTE: The progress bar need be called again to update the console informations
+	 */
+	CONSOLE_EXT_GRAPHIC_INTERFACE_LIB_API void DrawProgressBar(int progressBarXPos, int progressBarYPor, int maxWidth, int maxValue, int actualValue);
+
+	/**
+	 * @brief Draw a progress bar with a counter and the final value to be reached
+	 * @param progressBarXPos Define the progress bar position on X-axis
+	 * @param progressBarYPor Define the progress bar position on Y-axis
+	 * @param maxWidth Define the maximum characters will be used to progress bar and counter informations
+	 * @param maxValue Final value that need be reached
+	 * @param actualValue Actual value in progress bar. NOTE: The progress bar need be called again to update the console informations
+	 */
+	CONSOLE_EXT_GRAPHIC_INTERFACE_LIB_API void DrawProgressBarW(int progressBarXPos, int progressBarYPor, int maxWidth, int maxValue, int actualValue);
+}
+
+#endif //!CONSOLE_EXT_GRAPHIC_INTERFACE_HPP
